c5ea9f2c0cad63084a9a6b179ef880554b236685
이성준 <dhzm2aud@naver.com>
2020-02-24 09:07:29 +0900

    userLogin 어노테이션 만들기

---
+++ src/main/java/org/dhzm/book/springboot/config/WebConfig.java
@@ -0,0 +1,24 @@
+package org.dhzm.book.springboot.config;
+
+import lombok.RequiredArgsConstructor;
+import org.dhzm.book.springboot.config.auth.LoginUserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
+
+/*
+    HandlerMethodArgumentResolver는 항상 WebMvcConfigurer의 addArgumentResolvers()를 통해 추가
+ */
