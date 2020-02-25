90a44a88518cf457a54f5256b4390a6979c125bb
이성준 <dhzm2aud@naver.com>
2020-02-25 18:51:55 +0900

    test code에 security 보안 적용해서 테스트
    - test properties 적용
    - test security dependency 추가
    - jpa auditing를 spring boot application과 분리

--- src/main/java/org/dhzm/book/springboot/Application.java
+++ src/main/java/org/dhzm/book/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+//@EnableJpaAuditing  // JPA Auditing 삭제
 @SpringBootApplication  // 이 위치부터 설정을 읽어옴 -> 프로젝트 최상단에 위치
 public class Application {
     public static void main(String[] args) {
